44ddca6b21c96bb8ad1c4ee2828a896df0173aee
Elias da Rosa <elias@eliasdarosa.com.br>
2020-08-21 16:04:51 -0300

    added functionality

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,84 @@ app.use(cors());
 
 const repositories = [];
 
+// list
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
+
+// insert
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  //
+  repositories.push(repository);
+
+  //
+  return response.json(repository);
 });
 
+
+// update
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+
+  //
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const index = repositories.findIndex((repository) => {
+    return repository.id === id;
+  });
+
+  if (index === -1) {
+    return response.status(400).send();
+  }
+
+  //
+  repositories[index] = { ...repositories[index], title, url, techs };
+  return response.json(repositories[index]);
 });
 
+// delete
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+
+  //
+  const { id } = request.params;
+  const index = repositories.findIndex((repository) => {
+    return repository.id === id;
+  });
+
+  if (index === -1) {
+    return response.status(400).send();
+  }
+
+  //
+  repositories.splice(index, 1);
+  return response.status(204).send();
 });
 
+// insert like
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const index = repositories.findIndex((repository) => {
+    return repository.id === id;
+  });
+
+  if (index === -1) {
+    return response.status(400).send();
+  }
+
+
+  repositories[index].likes += 1;
+  return response.json(repositories[index]);
+
 });
 
 module.exports = app;
